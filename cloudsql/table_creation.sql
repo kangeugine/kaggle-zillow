b22181ada929a56472362783a48e3303b1752700
Eugine Kang <kangeugine@gmail.com>
2017-07-19 13:52:15 -0700

    add cloudsql

---
+++ cloudsql/table_creation.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS kaggle;
+
+USE kaggle;
+
+DROP TABLE IF EXISTS Properties;
+DROP TABLE IF EXISTS Train;
+CREATE TABLE IF NOT EXISTS Properties
+(
+  id varchar(255),
+  title varchar(255),
+  location varchar(255),
+  price int,
+  rooms int,
+  rating float,
+  type varchar(255),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE  IF NOT EXISTS Train
+(
+  parcelid int,
+  logerror float,
+  transactiondate varchar(255),
+  PRIMARY KEY parcelid
+);
+
+
